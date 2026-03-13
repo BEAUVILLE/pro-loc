6ec99c4ae0e367d960d3c842890593b61b6ef452
DIGIY LYFE <baptistejb24@gmail.com>
2026-03-13 20:28:09 +0000

    Update guard.js

--- guard.js
+++ guard.js
@@ -9,9 +9,7 @@
   const DEFAULT_DASHBOARD_URL = "./app.html";
 
   const FALLBACK_SUPABASE_URL = "https://wesqmwjjtsefyjnluosj.supabase.co";
-  const FALLBACK_SUPABASE_ANON_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indlc3Ftd2pqdHNlZnlqbmx1b3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNzg4ODIsImV4cCI6MjA4MDc1NDg4Mn0.dZfYOc2iL2_wRYL3zExZFsFSBK6AbMeOid2LrIjcTdA";
-
+const FALLBACK_SUPABASE_ANON_KEY = "sb_publishable_tGHItRgeWDmGjnd0CK1DVQ_BIep4Ug3";
   let _sb = null;
   let _bootPromise = null;
 
